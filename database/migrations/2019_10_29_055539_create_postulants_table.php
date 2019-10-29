994ec7e892312e818ef481b32fff48a0a9db0a6d
Andres Justiniano <andres95liverpool@gmail.com>
2019-10-29 02:30:28 -0400

    Base project

---
+++ database/migrations/2019_10_29_055539_create_postulants_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostulantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('postulants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('last name');
+            $table->string('email')->unique();
+            $table->integer('country'); 
+            $table->integer('city');
+
+            $table->string('direction');
+            $table->string('telephone');
+            $table->string('identity_card');
+            $table->date('birthday');
+            $table->integer('gender');
+            
+            
+            
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('postulants');
+    }
+}
